9ec10a6af61bb9388aee3840a590e2a9998249be
laurent <laurent.tempier@momagroup.com>
2015-06-30 19:53:07 +0200

    wip

--- app/scripts/controllers/buildings.controller.js
+++ app/scripts/controllers/buildings.controller.js
@@ -8,75 +8,90 @@
  * Controller of the sketchupApp
  */
 angular.module('sketchupApp')
-    .controller('BuildingsCtrl', ['$scope', '$http', 'leafletData', 'buildings', function ($scope, $http, leafletData, buildings) {
-        $scope.map = {
-            center: {
-                lat: 48.8597625,
-                lng: 2.3435408,
-                zoom: 16
-            },
-            tiles: {
-                url: 'http://api.tiles.mapbox.com/v4/{mapid}/{z}/{x}/{y}.png?access_token={apikey}',
-                type: 'xyz',
-                options: {
-                    apikey: 'pk.eyJ1IjoiYnVmYW51dm9scyIsImEiOiJLSURpX0pnIn0.2_9NrLz1U9bpwMQBhVk97Q',
-                    mapid: 'ltempier.k4ppao03'
+    .controller('BuildingsCtrl', ['$scope', '$http', '$timeout', 'leafletData', 'buildings',
+        function ($scope, $http, $timeout, leafletData, buildings) {
+            $scope.map = {
+                center: {
+                    lat: 48.859762,
+                    lng: 2.3435408,
+                    zoom: 18
+                },
+                tiles: {
+                    url: 'http://api.tiles.mapbox.com/v4/{mapid}/{z}/{x}/{y}.png?access_token={apikey}',
+                    type: 'xyz',
+                    options: {
+                        apikey: 'pk.eyJ1IjoiYnVmYW51dm9scyIsImEiOiJLSURpX0pnIn0.2_9NrLz1U9bpwMQBhVk97Q',
+                        mapid: 'ltempier.k4ppao03'
+                    }
                 }
-            }
-        };
+            };
 
-        $scope.start = function () {
-            leafletData.getMap().then(function (map) {
-                buildings.init();
-                var mapCenter = map.getCenter();
-                var mapZoom = map.getZoom();
-                var bounds = map.getBounds();
-                var box = [bounds.getSouth(), bounds.getWest(), bounds.getNorth(), bounds.getEast()];
-                var url = 'https://overpass-api.de/api/interpreter?data=[out:json];((way(' + box.join(',') + ')[%22building%22]);(._;node(w);););out;';
-                $http.get(url)
-                    .success(filterData);
+            $scope.start = function () {
+                $scope.loading = true;
+                leafletData.getMap().then(function (map) {
+                    buildings.init();
+                    var mapCenter = map.getCenter();
+                    var mapZoom = map.getZoom();
+                    var bounds = map.getBounds();
+                    var box = [bounds.getSouth(), bounds.getWest(), bounds.getNorth(), bounds.getEast()];
+                    var url = 'https://overpass-api.de/api/interpreter?data=[out:json];((way(' + box.join(',') + ')[%22building%22]);(._;node(w);););out;';
+                    $http.get(url)
+                        .success(filterData);
 
-                function filterData(data) {
-                    var nodes = _.filter(data.elements, function (item) {
-                        return 'node' === item.type;
-                    });
-                    var features = _.filter(data.elements, function (item) {
-                        return 'way' === item.type && item.tags.building;
-                    });
-
-                    _.each(features, function (feature) {
-                        var levels = feature.tags['building:levels'] || 2;
-                        var outlinePath = filterNodes(feature, nodes);
-                        var points = _.map(outlinePath, function (coord) {
-                            return convertProjection(coord)
+                    function filterData(data) {
+                        var nodes = _.filter(data.elements, function (item) {
+                            return 'node' === item.type;
                         });
-                        buildings.add(points, levels)
-                    });
+                        var features = _.filter(data.elements, function (item) {
+                            return 'way' === item.type && item.tags.building;
+                        });
 
-                    $scope.buildings = buildings.get()
-                }
+                        _.each(features, function (feature) {
+                            var levels = feature.tags['building:levels'] || 2;
+                            var outlinePath = filterNodes(feature, nodes);
+                            var points = _.map(outlinePath, function (coord) {
+                                return convertProjection(coord)
+                            });
+                            buildings.add(points, levels)
+                        });
 
-                function filterNodes(feature, nodes) {
-                    var path = _(nodes).chain().filter(function (node) {
-                        return 0 <= feature.nodes.indexOf(node.id);
-                    }).sortBy(function (node) {
-                        return feature.nodes.indexOf(node.id);
-                    }).map(function (node) {
-                        return [node.lon, node.lat];
-                    }).value();
-                    return path;
-                }
+                        $scope.buildings = buildings.get();
 
-                function convertProjection(coords) {
-                    var tileSize = 128; // Pixel size of a single map tile
-                    var zoom = mapZoom; // Zoom level
-                    var projection = d3.geo.mercator()
-                        .center([mapCenter.lng, mapCenter.lat]) // Geographic coordinates of map centre
-                        .translate([0, 0]) // Pixel coordinates of .center()
-                        .scale(tileSize << zoom); // Scaling value
-                    var pixelValue = projection(coords); // Returns [x, y]
-                    return [pixelValue[1] * -1, pixelValue[0] * -1];
-                }
-            })
-        };
-    }]);
+                        $timeout(function () {
+                            $scope.loading = false
+                        });
+                    }
+
+                    function filterNodes(feature, nodes) {
+                        var path = _(nodes).chain().filter(function (node) {
+                            return 0 <= feature.nodes.indexOf(node.id);
+                        }).sortBy(function (node) {
+                            return feature.nodes.indexOf(node.id);
+                        }).map(function (node) {
+                            return [node.lon, node.lat];
+                        }).value();
+                        return path;
+                    }
+
+                    function convertProjection(coords) {
+                        var tileSize = 128; // Pixel size of a single map tile
+                        var zoom = mapZoom; // Zoom level
+                        var projection = d3.geo.mercator()
+                            .center([mapCenter.lng, mapCenter.lat]) // Geographic coordinates of map centre
+                            .translate([0, 0]) // Pixel coordinates of .center()
+                            .scale(tileSize << zoom); // Scaling value
+                        var pixelValue = projection(coords); // Returns [x, y]
+                        return [pixelValue[1] * -1, pixelValue[0] * -1];
+                    }
+                })
+            };
+
+            $scope.download = function () {
+                if (!buildings) return;
+                var hiddenElement = document.createElement('a');
+                hiddenElement.href = encodeURI(buildings.toSketchupScript());
+                hiddenElement.target = '_blank';
+                hiddenElement.download = 'sketchup_script';
+                hiddenElement.click();
+            }
+        }]);
--- app/scripts/directives/three.directive.js
+++ app/scripts/directives/three.directive.js
@@ -4,25 +4,29 @@ angular.module('sketchupApp')
     .directive('threeScene', [function () {
         return {
             restrict: "E",
+            replace: true,
+            template: '<div class="threeScene"></div>',
             scope: {
                 blocks: "=blocks"
             },
-            link: function (scope, elem) {
+            link: function (scope, element, attrs) {
 
-                var renderer, camera, controls, scene;
+                element.css('width', attrs.width);
+                element.css('height', attrs.height);
+
+                var width, height, renderer, camera, controls, scene;
 
                 init();
+                animate();
 
                 function init() {
-
-                    console.log(elem.width() + ' x ' + elem.height())
-
-
+                    width = element.width();
+                    height = element.height();
                     renderer = new THREE.WebGLRenderer({antialias: true, alpha: true});
                     renderer.setClearColor('#FFFFFF', 0);
-                    renderer.setSize(elem.width(), elem.height());
+                    renderer.setSize(width, height);
 
-                    camera = new THREE.PerspectiveCamera(90, elem.width() / elem.height(), 0.1, 10000);
+                    camera = new THREE.PerspectiveCamera(90, width / height, 0.1, 10000);
                     camera.position.x = 10;
                     camera.position.y = 10;
                     camera.position.z = 10;
@@ -34,12 +38,69 @@ angular.module('sketchupApp')
                         render()
                     });
 
-                    scene = new THREE.Scene();
-                    scene.add(camera);
-                    elem[0].appendChild(renderer.domElement);
+                    element[0].appendChild(renderer.domElement);
+
+                    controls.handleResize();
+
+                    scope.$watch('blocks', function (blocks) {
+                        if (!blocks) return;
+                        scene = new THREE.Scene();
+                        scene.add(camera);
+                        _.each(blocks, addBlock);
+                        render()
+                    }, true);
+
+                    scope.$watch(function () {
+                        return [element[0].clientWidth, element[0].clientHeight].join('x');
+                    }, resize)
+
+                }
+
+                function addBlock(block) {
+                    var shape = new THREE.Shape();
+                    shape.moveTo(block.points[block.points.length - 1][0], block.points[block.points.length - 1][1]);
+                    _.each(block.points, function (xy) {
+                        shape.lineTo(xy[0], xy[1]);
+                    });
+                    var geom = new THREE.ExtrudeGeometry(shape, {
+                        amount: block.level,
+                        bevelEnabled: false,
+                        material: 0,
+                        extrudeMaterial: 1
+                    });
+                    var material = new THREE.LineBasicMaterial({
+                        color: self.randomColor ? randomColor() : self.colors.building,
+                        linewidth: 20,
+                        linecap: 'round',
+                        linejoin: 'round'
+                    });
+                    var mesh = new THREE.Mesh(geom, material);
+                    geom.computeFaceNormals();
+                    mesh.rotation.x = -Math.PI / 2;
+                    mesh.rotation.z = Math.PI / 2;
+                    mesh.castShadow = true;
+                    mesh.receiveShadow = true;
+                    scene.add(mesh);
+                }
+
+                function resize() {
+                    width = element.width();
+                    height = element.height();
+                    camera.aspect = width / height;
+                    camera.updateProjectionMatrix();
+                    renderer.setSize(width, height);
+                    controls.handleResize();
+                    render();
+                }
+
+
+                function animate() {
+                    requestAnimationFrame(animate);
+                    controls.update();
                 }
 
                 function render() {
+                    if (!scene) return;
                     renderer.render(scene, camera);
                 }
             }
